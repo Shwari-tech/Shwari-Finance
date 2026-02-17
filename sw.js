1e8bd7d0bf0db25c67c1184c44fc8c06820b39fb
Shwari Finance <246389718+Shwari-tech@users.noreply.github.com>
2026-02-17 15:12:20 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,75 @@
-// File: sw.js
-const CACHE_NAME = 'shwari-finance-v1';
+const CACHE_NAME = 'shwari-core-v4';
 
-// Files to save for offline use
-// NOTE: We do not cache the video because it is too large.
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './logo.png', 
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+// Add the URLs of the files you want to cache for offline access
+const CACHE_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css',
+  'https://cdn-icons-png.flaticon.com/512/2942/2942263.png'
 ];
 
-// 1. INSTALL: Cache the website shell
-self.addEventListener('install', (e) => {
+// 1. Install Event: Cache the application shell
+self.addEventListener('install', (event) => {
   console.log('[Service Worker] Installed');
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[Service Worker] Caching all assets');
-      return cache.addAll(ASSETS);
-    })
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('[Service Worker] Caching App Shell');
+        return cache.addAll(CACHE_ASSETS);
+      })
+      .then(() => self.skipWaiting())
   );
 });
 
-// 2. FETCH: Serve from cache if available, otherwise go to network
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
-});
-
-// 3. ACTIVATE: Clean up old caches if you update the version
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
+// 2. Activate Event: Clean up old caches if the version changes
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activated');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('[Service Worker] Clearing Old Cache:', cache);
+            return caches.delete(cache);
           }
         })
       );
     })
+  );
+  return self.clients.claim();
+});
+
+// 3. Fetch Event: Intercept network requests and serve from cache if offline
+self.addEventListener('fetch', (event) => {
+  // We only want to handle GET requests
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // If the network request is successful, clone the response and update the cache
+        const resClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          // Do not cache external cross-origin requests unless necessary, to save space
+          if (event.request.url.startsWith(self.location.origin)) {
+            cache.put(event.request, resClone);
+          }
+        });
+        return response;
+      })
+      .catch(() => {
+        // If network fails (offline), serve from cache
+        console.log('[Service Worker] Network failed, serving from cache');
+        return caches.match(event.request).then((cachedResponse) => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+          // If the page isn't in the cache, default to the home page shell
+          if (event.request.headers.get('accept').includes('text/html')) {
+            return caches.match('/');
+          }
+        });
+      })
   );
 });
